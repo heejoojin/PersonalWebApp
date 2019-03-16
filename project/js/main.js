4e03a23f7d73c691c5ae35dcadfd73c0e2fe8ebd
heejoojin <hjin77@gatech.edu>
2019-03-15 23:50:50 -0400

    firebase

--- project/js/main.js
+++ project/js/main.js
@@ -1,25 +1,87 @@
-var today = new Date();
+// Welcome!
 
-function copyright() {
-    var year = today.getFullYear();
 
-    var el = document.getElementById('cr');
-    el.innerHTML = '<p>Copyright &copy; ' + year + ' Muny Pottery Studio. All rights reserved.</p>'
-}
+//Read this file from bottom to top!!
 
-function greeting() {
-    var hourNow = today.getHours();
-    var greeting;
 
-    if (hourNow > 18) {
-        greeting = 'Good Evening !';
-    } else if ( hourNow > 12) {
-        greeting = 'Good Afternoon !';
-    } else if (hourNow > 0) {
-        greeting = 'Good Morning !';
+// This function sets up a listener- '.on()' gets called automatically whenever something saved in '/stream/' changes.
+// It's main purpose is to iterate over the stream in the database and add each message to the page.
+function initializeStreamListener() {
+  const databaseStreamReference = firebase.database().ref('/stream/');
+
+  databaseStreamReference.on('value', function(snapshot) {
+    var messages = snapshot.val();
+    $('#stream').empty();
+
+    if (messages) {
+      Object.keys(messages).forEach(function (key) {
+        const message = messages[key];
+        $('#stream').append(`<div>${message.body}</div>`);
+      });
     }
-    var el = document.getElementById('greeting');
-    el.textContent = greeting; 
+  });
 }
-greeting();
-copyright();
+
+// This function gets called with the new message information.
+// It gets the user information and uses both to add the post to the database.
+function addMessage(body, title) {
+  var user = firebase.auth().currentUser;
+  var authorPic = user.photoURL;
+  var author = user.displayName;
+
+  var postData = {
+    author: author,
+    authorPic: authorPic,
+    title: title,
+    body: body
+  };
+
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+}
+
+
+// This gets called whenver the form is submitted (check out the index.ejs).
+// Uses jQuery to get the message info and passes it to 'addMessage to actually submit the info to firebase.
+function handleMessageFormSubmit() {
+  var body = $('#new-post-body').val();
+  var title = $('#new-post-title').val();
+
+  addMessage(body, title);
+}
+
+// Gets called whenever the user clicks "sign in" or "sign out".
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { // if no user, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+
+  //This disables the button until login or logout is successful
+  $('#login-button').attr("disabled", true);
+}
+
+
+// The main purpose of this function is to set up a listener (using firebase) for when the auth state changes.
+// If a user isn't authenticated, we should not show the stream and prompt them to log in.
+// If a use IS authenticated, we should load/show the stream and give them the option to log out.
+window.onload = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      $('#stream').show();
+      $('#login-button').html("Log out");
+      initializeStreamListener();
+    } else {
+      $('#stream').hide();
+      $('#login-button').html("Log in with google");
+    }
+    $('#login-button').attr("disabled", false);
+  });
+};
