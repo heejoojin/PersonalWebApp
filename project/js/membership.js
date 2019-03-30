33cfb896d358a65db5f25a71403ef55384e100a2
heejoojin <hjin77@gatech.edu>
2019-03-30 16:53:10 -0400

    munystudio v1.0.3

--- project/js/membership.js
+++ project/js/membership.js
@@ -1,13 +1,56 @@
 function checkUsername() {                            // Declare function
-    var elMsg = document.getElementById('feedback'); // Get feedback element
+    var elMsg = document.getElementById('warning'); // Get feedback element
     if (this.value.length < 6) {                     // If username too short
-      elMsg.innerHTML = '<p><i class="fas fa-exclamation-triangle"></i> your username must be 6 characters or more</p>';  // Set msg
+      elMsg.innerHTML = '<p><i class="fas fa-exclamation-triangle"></i> your username must be 6 characters or more</p>';
+      /*var submit = document.getElementById('signupbtt');*/
     } else {                                         // Otherwise
       elMsg.innerHTML = '';                             // Clear message
     }
   }
   var elUsername = document.getElementById('usernameinput'); // Get username input
   elUsername.onblur = checkUsername;// when it loses focus call cehckuserName();
+
+/*function disablesubmit() {
+    var login = document.getElementById('signup');
+    var password = document.getElementById('password');
+    var submit = document.getElementById('signupbtt');
+    var submitted = false;
+    if (this.value.length < 6)
+}*/
+
+/*(function(){
+  var form      = document.getElementById('newPwd');  // The form
+  var password  = document.getElementById('pwd');     // Password input
+  var submit    = document.getElementById('submit');  // Submit button
+
+  var submitted = false;                            // Has form been submitted?
+  submit.disabled = true;                           // Disable submit button
+  submit.className = 'disabled';                    // Style submit button
+  console.log(submit.className);
+  
+  // On input: Check whether or not to enable the submit button
+  addEvent(password, 'input', function(e) {         // On input of password
+    var target = e.target || e.srcElement;          // Target of event
+    submit.disabled = submitted || !target.value;   // Set disabled property
+    // If form has been submitted or pwd has no value set CSS to disabled
+    submit.className = (submitted || !target.value) ? 'disabled' : 'enabled';
+  }); 
+
+  // On submit: Disable the form so it cannot be submitted again
+  addEvent(form, 'submit', function(e) {            // On submit
+    if (submit.disabled || submitted) {             // If disabled OR sent
+      e.preventDefault();                           // Stop form submitting
+      return;                                       // Stop processing function
+    }                                               // Otherwise continue...
+    submit.disabled = true;                         // Disable submit button
+    submitted = true;                               // Update submitted var
+    submit.className = 'disabled';                  // Update style
+
+    // Demo purposes only: What would have been sent & show submit is disabled
+    e.preventDefault();                             // Stop form submitting
+    alert('Password is ' + password.value);         // Show the text
+  });
+}());*/
 
 var el = 0;
 var username;
