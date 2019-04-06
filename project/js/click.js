4c30739691563a2cf9a0ef3f74192031bf664457
heejoojin <hjin77@gatech.edu>
2019-04-06 09:57:38 -0400

    Update click.js

--- project/js/click.js
+++ project/js/click.js
@@ -2,7 +2,7 @@
 var msg = '<div class=\"header\"><a id=\"close\" href="#"><h2 id="close">&#10005;<h2></a></div>';
 msg += '<div><h2>This Membership Webpage is Currently Under Construction</h2>';
 msg += 'we are in the process of updating sign&ndash;up and sing&ndash;in functionality!<br/>';
-msg += 'we recommend you to use your google account to join our membership :)<br/>thank you so muchfor your understanding <i class="fa fa-heart" id="heart" aria-hidden="true"></i></div>';
+msg += 'we recommend you to use your google account to join our membership :)<br/>thank you so much for your understanding <i class="fa fa-heart" id="heart" aria-hidden="true"></i></div>';
 
 var elNote = document.createElement('div');       // Create a new element
 elNote.setAttribute('id', 'note');                // Add an id of note
